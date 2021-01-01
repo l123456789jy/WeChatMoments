e1dc6ae373b0ab8ee882e3031725276350b55d9f
liujingyuan <906514731@qq.com>
2021-01-01 12:56:58 +0800

    add viewModelScope

--- app/src/main/java/com/liujingyuan/wechatmoments/viewmodel/WeChatViewModel.kt
+++ app/src/main/java/com/liujingyuan/wechatmoments/viewmodel/WeChatViewModel.kt
@@ -4,5 +4,8 @@ import com.liujingyuan.wechatmoments.http.HttpManger
 import com.liujingyuan.wechatmoments.viewmodel.base.BaseViewModel
 
 class WeChatViewModel : BaseViewModel() {
+    /*viewModelScope.launch  {
+        remoteApi.slowFetch()
+    }*/
     fun loadeUserInfo(userId: String) = HttpManger.httpApi.getUserInfo(userId)
 }
